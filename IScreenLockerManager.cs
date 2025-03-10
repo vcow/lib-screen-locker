3faf772513c56f3280d5fbd0491aab74cf88088d
jvirkovskiy <jvirkovskiy@2reallife.com>
2025-03-10 15:14:03 +0300

    Version 2.0.0.

--- IScreenLockerManager.cs
+++ IScreenLockerManager.cs
@@ -2,18 +2,6 @@ using System;
 
 namespace Plugins.vcow.ScreenLocker
 {
-	/// <summary>
-	/// The locker types.
-	/// </summary>
-	[Serializable]
-	public enum LockerType
-	{
-		Undefined,
-		GameLoader,
-		SceneLoader,
-		BusyWait
-	}
-
 	public delegate void IsLockedChangedHandler(IScreenLockerManager screenLockerManager, bool isLocked);
 
 	/// <summary>
@@ -34,22 +22,22 @@ namespace Plugins.vcow.ScreenLocker
 		/// <summary>
 		/// Enable a blocking of the specified type.
 		/// </summary>
-		/// <param name="type">The type of blocking.</param>
+		/// <param name="key">The type of blocking.</param>
 		/// <param name="completeCallback">A callback, which call when blocking is finished.</param>
-		void Lock(LockerType type, Action completeCallback);
+		/// <param name="args">Additional args to send to the locker.</param>
+		void Lock(string key, Action completeCallback = null, object[] args = null);
 
 		/// <summary>
 		/// Disable a current blocking.
 		/// </summary>
+		/// <param name="key">A type of the blocking to disable, if null all blocks are disabled.</param>
 		/// <param name="completeCallback">A callback, which call when unblocking is finished.</param>
-		/// <param name="type">A type of the blocking to disable, if null all blocks are disabled.</param>
-		void Unlock(Action<LockerType> completeCallback, LockerType? type = null);
+		void Unlock(string key = null, Action<string> completeCallback = null);
 
 		/// <summary>
 		/// Set a screen locker for the specified type.
 		/// </summary>
-		/// <param name="type">The type of locker.</param>
-		/// <param name="baseScreenLockerPrefab">A prefab of the screen locker window.</param>
-		void SetScreenLocker(LockerType type, BaseScreenLocker baseScreenLockerPrefab);
+		/// <param name="screenLockerBasePrefab">A prefab of the screen locker window.</param>
+		void SetScreenLocker(ScreenLockerBase screenLockerBasePrefab);
 	}
 }
